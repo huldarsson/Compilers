935573339dcf8edf540f1dac5e5a8d3e09301769
Róbert Ingi Huldarsson <robert.huldarsson@gmail.com>
2020-02-06 19:10:09 +0000

    commit

---
+++ Compiler1/nanolexer/test.s
@@ -0,0 +1,24 @@
+;;;Prufa
+fibo(n){
+  if(n < 2){
+    return 1;
+  } else {
+    return fibo(n - 1) + fibo(n - 2);
+  }
+}
+
+while(x < 3){
+  print("if");
+}
+
+var t = 2;
+var test3;
+var 4test = test;
+test+
++123
++test
+
+'xx'
+'x'
+!%&/=-?^+*~<>|
+`
